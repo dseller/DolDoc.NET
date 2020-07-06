db6816ed214f379198af9d0e53503dff9ccdcb1c
Dennis Seller <d.seller93@gmail.com>
2020-07-06 18:53:13 +0200

    Added CH command for printing chars, added charmap doldoc file

--- DolDoc.Editor/Entries/EntryFactory.cs
+++ DolDoc.Editor/Entries/EntryFactory.cs
@@ -11,6 +11,7 @@ namespace DolDoc.Editor.Entries
             {
                 case "BG": return new Background(flags, args);
                 case "BK": return new Blink(flags, args);
+                case "CH": return new Char(flags, args);
                 case "CL": return new Clear(flags, args);
                 case "CM": return new CursorMove(flags, args);
                 case "ER": return new Error(null);
