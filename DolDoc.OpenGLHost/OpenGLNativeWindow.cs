9fa446f43a723a8ade4171c17a9404f67cfc203c
Dennis Seller <d.seller93@gmail.com>
2021-09-06 18:22:41 +0200

    OpenGL.NET to OpenTK

--- DolDoc.OpenGLHost/OpenGLNativeWindow.cs
+++ DolDoc.OpenGLHost/OpenGLNativeWindow.cs
@@ -2,8 +2,11 @@
 using DolDoc.Editor.Core;
 using DolDoc.Editor.Fonts;
 using DolDoc.Editor.Rendering;
-using OpenGL;
-using OpenGL.CoreUI;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
 using Serilog;
 using System;
 using System.Collections.Generic;
@@ -13,42 +16,18 @@ namespace DolDoc.OpenGLHost
 {
     public class OpenGLNativeWindow : IFrameBufferWindow, IDisposable
     {
-        private readonly Thread thread;
         private Timer timer;
-        private readonly NativeWindow nativeWindow;
+        private Thread thread;
         private Document document;
         private ViewerState viewerState;
-        private EgaColor[] _framebuffer;// = new EgaColor[1024 * 768];
+        private EgaColor[] _framebuffer;
         private object framebufferLock = new object();
-        private int width, height;
 
-        public OpenGLNativeWindow()
-        {
-            nativeWindow = NativeWindow.Create();
-            thread = new Thread(Run);
-            // 
-        }
+        private NativeWindow nativeWindow;
+        private IGraphicsContext graphicsContext;
+
 
         public void Clear() => Array.Fill(_framebuffer, EgaColor.Palette[15]);
-
-        private void Run()
-        {
-            Log.Verbose("Starting OpenGLNativeWindow thread...");
-
-            nativeWindow.ContextCreated += NativeWindow_ContextCreated;
-            nativeWindow.Render += NativeWindow_Render;
-            nativeWindow.KeyDown += NativeWindow_KeyDown;
-            nativeWindow.Create(0, 0, (uint)width, (uint)height, NativeWindowStyle.None);
-            nativeWindow.Show();
-
-            viewerState.Pages.Clear();
-            document.Refresh();
-
-            timer = new Timer(_ => viewerState.Tick(), null, 0, 200);
-            nativeWindow.Run();
-        }
-
-        public void Dispose() => nativeWindow.Dispose();
 
         public void Render(byte[] data)
         {
@@ -63,58 +42,93 @@ namespace DolDoc.OpenGLHost
 
         public void Show(string title, int width, int height, Document document = null)
         {
-            this.width = width;
-            this.height = height;
-            this.document = document ?? new Document(width / 8, height / 8);
-            viewerState = new ViewerState(this, this.document, width, height, new YaffFontProvider(), "Courier_8_2");
-            // Log.Verbose("Document content: {0}", this.document.ToPlainText());
+            thread = new Thread(() =>
+            {
+                Log.Verbose("Starting OpenGLNativeWindow thread...");
+                _framebuffer = new EgaColor[width * height];
 
-            _framebuffer = new EgaColor[width * height];
+                nativeWindow = new NativeWindow(new NativeWindowSettings
+                {
+                    Size = new Vector2i(width, height),
+                    Profile = ContextProfile.Compatability,
+                    WindowBorder = WindowBorder.Hidden,
+                    IsEventDriven = false
+                });
+
+
+                unsafe
+                {
+                    graphicsContext = new GLFWGraphicsContext(nativeWindow.WindowPtr);
+                }
+
+                nativeWindow.CenterWindow();
+                this.document = document ?? new Document(width / 8, height / 8);
+                viewerState = new ViewerState(this, this.document, width, height, new YaffFontProvider(), "Courier_8_2");
+
+                viewerState.Pages.Clear();
+                document.Refresh();
+                timer = new Timer(_ => viewerState.Tick(), null, 0, 200);
+                
+                OnLoad();
+                nativeWindow.KeyDown += NativeWindow_KeyDown;
+
+                while (nativeWindow.Exists)
+                {
+                    nativeWindow.ProcessEvents();
+                    // TODO: fps restrict
+                    OnRenderFrame();
+                    Thread.Sleep(1);
+                }
+            });
+
             thread.Start();
         }
 
-        private void NativeWindow_Render(object sender, NativeWindowEventArgs e)
+        private void NativeWindow_KeyDown(KeyboardKeyEventArgs e)
         {
-            NativeWindow nativeWindow = (NativeWindow)sender;
-            Gl.Viewport(0, 0, (int)nativeWindow.Width, (int)nativeWindow.Height);
-            Gl.Clear(ClearBufferMask.ColorBufferBit);
-            Gl.RasterPos2(-1, 1);
-            Gl.PixelZoom(1, -1);
-
-            lock (framebufferLock)
-                Gl.DrawPixels(1024, 768, PixelFormat.Rgb, PixelType.UnsignedByte, _framebuffer);
-        }
-
-        private static void NativeWindow_ContextCreated(object sender, NativeWindowEventArgs e)
-        {
-            Gl.ReadBuffer(ReadBufferMode.Back);
-            Gl.ClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-            Gl.Enable(EnableCap.Blend);
-            Gl.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
-            Gl.LineWidth(2.5f);
-        }
-
-        private void NativeWindow_KeyDown(object sender, NativeWindowKeyEventArgs e)
-        {
-            if (e.Key == KeyCode.Escape)
+            if (e.Key == Keys.Escape)
                 Environment.Exit(0);
 
-            var keyDownTranslation = new Dictionary<KeyCode, ConsoleKey>
+            var keyDownTranslation = new Dictionary<Keys, ConsoleKey>
             {
-                { KeyCode.Down, ConsoleKey.DownArrow},
-                { KeyCode.Right, ConsoleKey.RightArrow },
-                { KeyCode.Left, ConsoleKey.LeftArrow },
-                { KeyCode.Up, ConsoleKey.UpArrow },
+                { Keys.Down, ConsoleKey.DownArrow},
+                { Keys.Right, ConsoleKey.RightArrow },
+                { Keys.Left, ConsoleKey.LeftArrow },
+                { Keys.Up, ConsoleKey.UpArrow },
                 //{ KeyCode., ConsoleKey.Backspace },
-                { KeyCode.Delete, ConsoleKey.Delete },
-                { KeyCode.Home, ConsoleKey.Home },
+                { Keys.Delete, ConsoleKey.Delete },
+                { Keys.Home, ConsoleKey.Home },
                 //{ KeyCode., ConsoleKey.PageUp },
-                //{ KeyCode.PageDown, ConsoleKey.PageDown }
-                { KeyCode.Space, ConsoleKey.Spacebar }
+                { Keys.PageDown, ConsoleKey.PageDown },
+                { Keys.Space, ConsoleKey.Spacebar },
             };
 
             if (keyDownTranslation.TryGetValue(e.Key, out var key))
                 viewerState.KeyDown(key);
         }
+
+        private void OnLoad()
+        {
+            GL.ReadBuffer(ReadBufferMode.Back);
+            GL.ClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+            GL.Enable(EnableCap.Blend);
+            GL.BlendFunc(BlendingFactor.SrcAlpha, BlendingFactor.OneMinusSrcAlpha);
+            GL.LineWidth(2.5f);
+        }
+
+        private void OnRenderFrame()
+        {
+            GL.Viewport(0, 0, nativeWindow.Size.X, nativeWindow.Size.Y);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            GL.RasterPos2(-1, 1);
+            GL.PixelZoom(1, -1);
+
+            lock (framebufferLock)
+                GL.DrawPixels(1024, 768, PixelFormat.Rgb, PixelType.UnsignedByte, _framebuffer);
+
+            graphicsContext.SwapBuffers();
+        }
+
+        public void Dispose() => nativeWindow.Close();
     }
 }
--- DolDoc.OpenGLHost/Program.cs
+++ DolDoc.OpenGLHost/Program.cs
@@ -16,7 +16,7 @@ namespace DolDoc.OpenGLHost
 
             var compositor = new Compositor<OpenGLNativeWindow>();
             var window = compositor.NewWindow();
-            window.Show("Demo", 1024, 768, DocumentLoader.Load(File.Open("Examples/GraphicsOverview.DD", FileMode.Open), 128, 73));
+            window.Show("Demo", 1024, 768, DocumentLoader.Load(File.Open("Examples/GraphicsOverview.DD", FileMode.Open), 128, 76));
 
             //var window2 = compositor.NewWindow();
             //window2.Show("Another", 640, 480, DocumentLoader.Load(File.Open("Examples/Job.DD", FileMode.Open)));
